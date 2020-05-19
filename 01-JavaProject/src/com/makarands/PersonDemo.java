6edbb3057999742672e60ceaa6e9498da2d476d4
makarandbhoir <bhoir.makarand@gmail.com>
2020-05-19 20:24:37 +0530

    added score

--- 01-JavaProject/src/com/makarands/PersonDemo.java
+++ 01-JavaProject/src/com/makarands/PersonDemo.java
@@ -4,5 +4,6 @@ public class PersonDemo {
 	public static void main(String[] args) {
 		Person ram = new Person("Ram", 36, 70.5);
 		System.out.println("Name: "+ram.getPersonName());
+		System.out.println("Score: "+ram.getPersonScore());
 	}
 }

352eba4eb4b0b27c9f72622168b99e1fc42e5e42
makarandbhoir <bhoir.makarand@gmail.com>
2020-05-19 20:21:54 +0530

    removed extra line

--- 01-JavaProject/src/com/makarands/PersonDemo.java
+++ 01-JavaProject/src/com/makarands/PersonDemo.java
@@ -3,7 +3,6 @@ package com.makarands;
 public class PersonDemo {
 	public static void main(String[] args) {
 		Person ram = new Person("Ram", 36, 70.5);
-		
 		System.out.println("Name: "+ram.getPersonName());
 	}
 }
